a8fa714fefe99cab2635cd9c594a78dc520a9ef9
mayankpallai <mayankpallai@gmail.com>
2024-10-11 00:44:00 +0800

    add footer code

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import HomePage from "./pages/HomePage"
 
 export default function App() {
   return (
-    <>
     <HomePage></HomePage>
-    </>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Navbar from "@/components/user/navbar";
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import CarouselSlides from "@/components/common/CarouselSlides";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <Navbar/>
+      <Navbar />
       <div className="flex-grow">
+        <CarouselSlides />
       </div>
+      <Footer />
     </div>
   );
 };
